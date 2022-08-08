64fba98c7b2cd81034ccdfa2ba45cd168a1c18f6
Weslie0803 <zhengsx19@mails.tsinghua.edu.cn>
2022-08-08 17:23:44 +0800

    iinit

---
+++ code.js
@@ -0,0 +1,8 @@
+function Click(){
+    // alert("生日快乐");
+    document.getElementById('1').style.color = 'red';
+    document.getElementById('2').style.color = 'orange';
+    document.getElementById('3').style.color = 'yellow';
+}
+
+Click();
---
+++ localserver/readme.md
@@ -0,0 +1,8 @@
+# 介绍
+运行简单的本地http服务器，使用python的SimpleHTTPServer模块。
+
+running simple local http server by `SimpleHTTPServer` module of python
+
+默认端口为8000
+
+default port is 8000
